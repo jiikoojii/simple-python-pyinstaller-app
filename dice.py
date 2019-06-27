8eee681ecaa4646bdfe7caa74ee05b72626eb0a9
Jani Järvinen <jani.jarvinen@qvantel.com>
2019-06-27 15:44:33 +0300

    randomized calculator

--- dice.py
+++ dice.py
@@ -4,12 +4,13 @@ max = 1000
 
 x = random.randint(min, max)
 y = random.randint(min, max)
+z = random.randint(1, 4)
 #if (number < 50):
 #	print("Okay so basically am very smol only ", #number)
 #else:
 #	print("Not so smol anymoar with ", number)
 #print(number)
-
+print(z, y, z)
 def add(x, y):
    return x + y
 # This function subtracts two numbers 
@@ -21,8 +22,12 @@ def multiply(x, y):
 # This function divides two numbers
 def divide(x, y):
    return x / y
-   
-print(add(x, y))
-print(subtract(x, y))
-print(multiply(x, y))
-print(divide(x, y))
+
+if z == 1:
+	print(add(x, y))
+elif z == 2:	
+	print(subtract(x, y))
+elif z == 3:	
+	print(multiply(x, y))
+elif z == 4:	
+	print(divide(x, y))
