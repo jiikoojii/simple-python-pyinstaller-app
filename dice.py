fe3cd73ec69e17624168fd3bd09a33e307ba8f58
Jani Järvinen <jani.jarvinen@qvantel.com>
2019-06-27 15:27:59 +0300

    Number visible

--- dice.py
+++ dice.py
@@ -4,6 +4,6 @@ max = 101
 
 number = random.randint(min, max)
 if (number < 50):
-	print("Okay so basically am very smol")
+	print("Okay so basically am very smol only " + number)
 else:
-	print("Not so smol anymoar")
+	print("Not so smol anymoar with " + number)
