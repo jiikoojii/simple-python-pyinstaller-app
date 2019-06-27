cf172f1801615b527c5e9682aab09449ac3849a3
Jani Järvinen <jani.jarvinen@qvantel.com>
2019-06-27 15:29:04 +0300

    took + away

--- dice.py
+++ dice.py
@@ -4,6 +4,6 @@ max = 101
 
 number = random.randint(min, max)
 if (number < 50):
-	print("Okay so basically am very smol only " + number)
+	print("Okay so basically am very smol only " number)
 else:
-	print("Not so smol anymoar with " + number)
+	print("Not so smol anymoar with " number)
