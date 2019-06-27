f0bc9e34dfbc7e84408a1250a86c370108cb24f3
Jani Järvinen <jani.jarvinen@qvantel.com>
2019-06-27 15:36:17 +0300

    Calculatorish

--- dice.py
+++ dice.py
@@ -1,10 +1,28 @@
 import random
-min = 1
-max = 101
+min = 0
+max = 1000
 
-number = random.randint(min, max)
-if (number < 50):
-	print("Okay so basically am very smol only ", number)
-else:
-	print("Not so smol anymoar with ", number)
-print(number)
+x = random.randint(min, max)
+y = random.randint(min, max)
+#if (number < 50):
+#	print("Okay so basically am very smol only ", #number)
+#else:
+#	print("Not so smol anymoar with ", number)
+#print(number)
+
+def add(x, y):
+   return x + y
+# This function subtracts two numbers 
+def subtract(x, y):
+   return x - y
+# This function multiplies two numbers
+def multiply(x, y):
+   return x * y
+# This function divides two numbers
+def divide(x, y):
+   return x / y
+   
+print(add)
+print(subtract)
+print(multiply)
+print(divide)
